f49a51d266a2d6a67e31e67a38ea9c8b16992d43
Dmitriy Trandin <dmitriy.trandin@gmail.com>
2020-03-27 18:20:07 +0300

    add something 2

--- src/org/mailz/Main.java
+++ src/org/mailz/Main.java
@@ -19,6 +19,8 @@ public static void main(String args[]) throws StackException {
 
     stack.push("dsadas");
     stack.push("dsadas");
+    stack.push("dsadas");
+    stack.push("dsadas");
 
 
     System.out.println();
