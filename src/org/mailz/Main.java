320128b96f5749e2d0f3d03af34f70c8465e619c
Dmitriy <dmitriy.trandin@gmail.com>
2020-03-30 14:30:27 +0300

    Merge pull request #16 from mailz/feature/f1
    Feature/f1

--- src/org/mailz/Main.java
+++ src/org/mailz/Main.java
@@ -13,10 +13,22 @@ public static void main(String args[]) throws StackException {
     System.out.println(stack.isFull());
     System.out.println(stack.isEmpty());
     stack.push("dsadas");
+    stack.push("dsadas");
+    stack.push("dsadas");
+    stack.push("dsadas");
+    stack.push("dsadas");
+    stack.push("dsadas");
     System.out.println();
 
     System.out.println(stack.isFull());
     System.out.println(stack.isEmpty());
+
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+
 
 }
 
