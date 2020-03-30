cf422f14664eb8fc6e8803e15b799fea5875a460
Dmitriy Trandin <dmitriy.trandin@gmail.com>
2020-03-30 14:17:00 +0300

    add something 01

--- src/org/mailz/Main.java
+++ src/org/mailz/Main.java
@@ -21,7 +21,7 @@ public static void main(String args[]) throws StackException {
     System.out.println(stack.isEmpty());
     System.out.println(stack.pop());
     System.out.println(stack.pop());
-    System.out.println(stack.pop());
+
 
 
 }
