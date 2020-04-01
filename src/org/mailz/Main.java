70fb420e5d697284b251dc2ae831734a37a1134d
Dmitriy Trandin <dmitriy.trandin@gmail.com>
2020-04-01 18:16:47 +0300

    test 12

--- src/org/mailz/Main.java
+++ src/org/mailz/Main.java
@@ -21,6 +21,10 @@ public static void main(String args[]) throws StackException {
     stack.push("dsadas");
     System.out.println(stack.isFull());
     System.out.println(stack.isEmpty());
+    System.out.println(stack.isEmpty());
+
+    System.out.println(stack.isEmpty());
+
 
 }
 
