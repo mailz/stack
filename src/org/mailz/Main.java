dec546ef9738880f7f9c358a26adaf596804a80b
Dmitriy Trandin <dmitriy.trandin@gmail.com>
2020-04-01 12:08:32 +0300

    test 11

--- src/org/mailz/Main.java
+++ src/org/mailz/Main.java
@@ -19,8 +19,6 @@ public static void main(String args[]) throws StackException {
     System.out.println();
     stack.push("dsadas");
     stack.push("dsadas");
-    stack.push("dsadas");
-    stack.push("dsadas");
     System.out.println(stack.isFull());
     System.out.println(stack.isEmpty());
 
